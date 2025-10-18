56fdff51c67fd0568cdf5742a0ba782242e993fb
NaveenPodilikuri <naveenpodilikuri@gmail.com>
2025-10-19 01:51:11 +0530

    Setup app, file and folder structure, routing, and Auth pages

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {runAfterProductionCompile} from "next/dist/build/after-production-compile";
+const Page=()=>{
+   return (
+       <div>Home Page</div>
+   )
+}
+export default Page
